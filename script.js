ceb1baa64afd70593b10150f2d992527e85b8de2
TranHieu <nguyentranhieugttn@gmail.com>
2022-09-23 00:23:02 +0700

    #1

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 let moveX = 20;
 let speed = 20;
 let bx = 250;
 let by = 250;
 let xv, yv;
-
+let target;
 // load canvas
 canvas = document.getElementById("gameCanvas");
 context = canvas.getContext("2d");
@@ -12,19 +11,53 @@ context = canvas.getContext("2d");
 context.fillStyle = "white";
 context.fillRect(0, 0, canvas.width, canvas.height);
 //vẽ quả bóng
-function Circle(x, y, radius) {
-  this.x = x;
-  this.y = y;
-  this.radius = radius;
-}
+// function Circle(x, y, radius) {
+//   this.x = x;
+//   this.y = y;
+//   this.radius = radius;
+// }
 function createCircle() {
-  let circle = new Circle(250, 250, 15);
+  // let circle = new Circle(250, 250, 15);
+}
+// cho bong di chuyen
+
+// random toc do ball
+xv = Math.floor(Math.random() * 76 + 25) / 30;
+yv = Math.floor(Math.random() * 76 + 25) / 30;
+
+// const myInterval = setInterval(update, 100);
+function update() {
+  bx += xv;
+  by += yv;
+  // trai
+  if (bx - speed / 2 < 0 && xv < 0) {
+    xv = -xv;
+  }
+  //phai
+  if (bx + speed / 2 > canvas.width && xv > 0) {
+    xv = -xv;
+  }
+  // tren
+  if (by - speed / 2 < 0 && yv < 0) {
+    yv = -yv;
+  }
+  //duoi
+  if (by + speed / 2 > canvas.height && yv > 0) {
+    yv = -yv;
+    clearInterval(myInterval);
+    // alert("ban da thua")
+  }
+  // khi va cham ball vs bar
+  if (395 <= by + speed / 2 ){
+    yv = -yv;
+  }
+  console.log(by + speed)
+  console.log(moveX)
   context.fillStyle = "black";
   context.beginPath();
-  context.arc(bx, by, circle.radius, 0, 2 * Math.PI);
+  context.arc(bx, by, 15, 0, 2 * Math.PI);
   context.fill();
 }
-
 // vẽ thanh ngang
 function Rectangle(x, y, width, height) {
   this.x = x;
@@ -46,37 +79,30 @@ function moveSelection(evt) {
       keyleft();
       break;
     case "ArrowRight":
-        keyright();
+      keyright();
       break;
   }
 }
 function keyright() {
-  if (moveX < 400){
-      context.clearRect(moveX, 400, 100, 10);
-      context.fillStyle = "blue";
-      moveX += speed;
-      context.fillRect(moveX, 400, 100, 10);
+  if (moveX < 400) {
+    context.clearRect(moveX, 400, 100, 10);
+    context.fillStyle = "blue";
+    moveX += speed;
+    context.fillRect(moveX, 400, 100, 10);
   }
 }
 function keyleft() {
-    if (moveX > 0){
-        context.clearRect(moveX, 400, 100, 10);
-        context.fillStyle = "blue";
-        moveX -= speed;
-        context.fillRect(moveX, 400, 100, 10);
-    }
-
+  if (moveX > 0) {
+    context.clearRect(moveX, 400, 100, 10);
+    context.fillStyle = "blue";
+    moveX -= speed;
+    context.fillRect(moveX, 400, 100, 10);
+  }
 }
-// cho bong di chuyen
 
-// function update() {
-//     xv = Math.floor(Math.random() * 76 + 25) / 30;
-//     yv = Math.floor(Math.random() * 76 + 25) / 30;
-//     bx += xv;
-//     by += yv;
-//   }
 function doReady() {
   window.addEventListener("keydown", moveSelection);
   createRectangle();
   createCircle();
+  update();
 }
