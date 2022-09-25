cb782a18d3a6c80f97edee0d38c61891c333c293
TranHieu <nguyentranhieugttn@gmail.com>
2022-09-26 00:10:16 +0700

    update file

--- bar.js
+++ bar.js
@@ -21,39 +21,39 @@ class Bar {
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   move() {
-    var a = this.x;
+    var coordinatesX = this.x;
     var speed = this.speed;
     window.addEventListener("keydown", function moveSelection(evt) {
       // clearInterval(myInterval);
       switch (evt.key) {
         case "ArrowRight":
-          if (a < 400) {
+          if (coordinatesX < 400) {
             document
               .getElementById("gameCanvas")
               .getContext("2d")
-              .clearRect(a, 400, 100, 10);
+              .clearRect(coordinatesX, 400, 100, 10);
             document.getElementById("gameCanvas").getContext("2d").fillStyle =
               this.color;
-            a += speed;
+            coordinatesX += speed;
             document
               .getElementById("gameCanvas")
               .getContext("2d")
-              .fillRect(a, 400, 100, 10);
+              .fillRect(coordinatesX, 400, 100, 10);
           }
           break;
         case "ArrowLeft":
-          if (a > 0) {
+          if (coordinatesX > 0) {
             document
               .getElementById("gameCanvas")
               .getContext("2d")
-              .clearRect(a, 400, 100, 10);
+              .clearRect(coordinatesX, 400, 100, 10);
             document.getElementById("gameCanvas").getContext("2d").fillStyle =
               this.color;
-            a -= speed;
+            coordinatesX -= speed;
             document
               .getElementById("gameCanvas")
               .getContext("2d")
-              .fillRect(a, 400, 100, 10);
+              .fillRect(coordinatesX, 400, 100, 10);
           }
           break;
       }
